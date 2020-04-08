9bd042e3015e743bfe02b8f4bb5265b89b766de4
Patrick Wang <wangchengbin_2009@vip.163.com>
2020-04-08 17:34:33 +0800

    小程序

--- web/controllers/account/account.py
+++ web/controllers/account/account.py
@@ -6,8 +6,10 @@ Create time: 2020-04-06
 IDE: PyCharm
 Introduction: 
 """
-from flask import Blueprint
-from common.libs.helper import ops_render
+from application import app, db
+from flask import Blueprint, request
+from common.libs.helper import ops_render, iPagination
+from common.models.user import User
 
 
 route_account = Blueprint('account_page', __name__)
@@ -15,7 +17,26 @@ route_account = Blueprint('account_page', __name__)
 
 @route_account.route('/index')
 def index():
-    return ops_render('account/index.html')
+    resp_data = {}
+    req = request.values
+    page = int(req['page']) if ('page' in req and req['page']) else 1
+    query = User.query
+
+    page_params = {
+        'total': query.count(),
+        'page_size': app.config['PAGE_SIZE'],
+        'page': page,
+        'display': app.config['PAGE_DISPLAY'],
+        'url': '/account/index'
+    }
+
+    pages = iPagination(page_params)
+    offset = (page - 1) * app.config['PAGE_SIZE']
+    limit = app.config['PAGE_SIZE'] * page
+    List = query.order_by(User.uid.desc()).all()[offset:limit]
+    resp_data['list'] = List
+    resp_data['pages'] = pages
+    return ops_render('account/index.html', resp_data)
 
 
 @route_account.route('/info')
