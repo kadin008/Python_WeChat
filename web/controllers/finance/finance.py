e503254a94856e55011e47fb2f05f34712337036
Patrick Wang <wangchengbin_2009@vip.163.com>
2020-04-08 17:04:43 +0800

    小程序

--- web/controllers/finance/finance.py
+++ web/controllers/finance/finance.py
@@ -6,24 +6,25 @@ Create time: 2020-04-06
 IDE: PyCharm
 Introduction: 
 """
-from flask import Blueprint, render_template
+from flask import Blueprint
+from common.libs.helper import ops_render
 
 route_finance = Blueprint('finance_page', __name__)
 
 
 @route_finance.route('/index')
 def index():
-    return render_template('finance/index.html')
+    return ops_render('finance/index.html')
 
 
 @route_finance.route('/pay-info')
 def payInfo():
-    return render_template('finance/pay_info.html')
+    return ops_render('finance/pay_info.html')
 
 
 @route_finance.route('/account')
 def account():
-    return render_template('finance/account.html')
+    return ops_render('finance/account.html')
 
 
 
