7048f1f351750841ddbbb8a652ed4933a664d171
Patrick Wang <wangchengbin_2009@vip.163.com>
2020-04-19 13:40:30 +0800

    小程序

--- common/models/food/wx_share_history.py
+++ common/models/food/wx_share_history.py
@@ -8,6 +8,6 @@ class WxShareHistory(db.Model):
     __tablename__ = 'wx_share_history'
 
     id = db.Column(db.Integer, primary_key=True)
-    member_id = db.Column(db.Integer, nullable=False, server_default=db.FetchedValue(), info='??id')
-    share_url = db.Column(db.String(200), nullable=False, server_default=db.FetchedValue(), info='?????url')
-    created_time = db.Column(db.DateTime, nullable=False, server_default=db.FetchedValue(), info='????')
+    member_id = db.Column(db.Integer, nullable=False, server_default=db.FetchedValue(), info='会员id')
+    share_url = db.Column(db.String(200), nullable=False, server_default=db.FetchedValue(), info='分享的页面url')
+    created_time = db.Column(db.DateTime, nullable=False, server_default=db.FetchedValue(), info='创建时间')
